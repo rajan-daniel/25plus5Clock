0a6bf10592a1181b88b91ddd67dc8572d0c38ca2
Rajan Daniel <rajan.daniel.dev@gmail.com>
2026-04-06 22:22:57 -0500

    feat: functional timer with clock format

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,9 +1,14 @@
 import { useState, useRef } from "react";
 
 export const Clock = () => {
-    const running = useRef(null);
+    const running = useRef(false);
+
+    if (running.current === false) {
+        console.log(running.current);
+    }
 
     const intervalRef = useRef(null);
+    const [minutes, setMinutes] = useState(25)
     const [time, setTime] = useState(1500)
 
     const start = () => {
@@ -23,13 +28,21 @@ export const Clock = () => {
         running.current = false;
         console.log(running.current);
         clearInterval(intervalRef.current);
+        setMinutes(25);
         setTime(1500);
+    }
+
+    function formatTime(seconds) {
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+
+        return `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
     }
 
     return (
         <>
-            <TimeControl time={time} setTime={setTime} isRunning={running} />
-            <h1>{time}</h1>
+            <TimeControl minutes={minutes} setMinutes={setMinutes} setTime={setTime} isRunning={running} />
+            <h1>{formatTime(time)}</h1>
             <button onClick={start}>Start</button>
             <button onClick={pause}>Stop</button>
             <button onClick={reset}>Reset</button>
@@ -37,13 +50,13 @@ export const Clock = () => {
     );
 }
 
-export const TimeControl = ({ time, setTime, isRunning }) => {
+export const TimeControl = ({ minutes, setMinutes, setTime, isRunning }) => {
     return (
         <>
             <h1>Session Length</h1>
-            <p>{time}</p>
-            <button onClick={() => { if (isRunning.current === false) { setTime((prev) => prev + 60) } }}>Increment</button>
-            <button onClick={() => { if (isRunning.current === false) { setTime((prev) => prev - 60) } }}>Decrement</button>
+            <p>{minutes}</p>
+            <button onClick={() => { if (isRunning.current === false) { setTime((minutes + 1) * 60); setMinutes((prev) => prev + 1); } }}>Increment</button>
+            <button onClick={() => { if (isRunning.current === false) { setTime((minutes - 1) * 60); setMinutes((prev) => prev - 1); } }}>Decrement</button>
         </>
     );
 }
